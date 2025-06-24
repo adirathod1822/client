1f6f8ebddf18d4a9eb8313245041815481d90833
adirathod1822 <adirathod1822@gmail.com>
2025-06-24 17:47:34 +0530

    hosting

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBJjNBYnvJV_0shk3f2iOfqL_LuoKsv0B8",
-  authDomain: "rex-chat-b6ef9.firebaseapp.com",
-  projectId: "rex-chat-b6ef9",
-  storageBucket: "rex-chat-b6ef9.firebasestorage.app",
-  messagingSenderId: "150899280738",
-  appId: "1:150899280738:web:8d27f99116a3fc64143a3b",
-  measurementId: "G-R05VJ0R7WS"
+  apiKey: "AIzaSyD16lF3cKk-pViLq5-zc-PlEvlJkPjc-ps",
+  authDomain: "onlychat-1f5ee.firebaseapp.com",
+  projectId: "onlychat-1f5ee",
+  storageBucket: "onlychat-1f5ee.firebasestorage.app",
+  messagingSenderId: "557767544646",
+  appId: "1:557767544646:web:fefd5bb40bff9d4f7d5f47",
+  measurementId: "G-5X73W82BZG"
 };
 
 const app = initializeApp(firebaseConfig);
